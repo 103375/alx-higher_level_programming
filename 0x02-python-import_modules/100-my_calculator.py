86a7590f64dccdaa6dc54b4a817c102cf0a92375
root <thembamashabane0@gmail.com>
2022-12-04 11:20:41 -0800

    python modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-from calculator_1 import sub, add, mul, div
-from sys import argv
+
 if __name__ == "__main__":
-    if len(argv) != 4:
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
 
-    signs = "+-x/"
-    SignByFun = {"-": sub, "+": add, "x": mul, "/": div}
-    a = int(argv[1])
-    b = int(argv[3])
-    op = argv[2]
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-    if op in signs:
-        print("{:d} {} {:d} = {:d}".format(a, op, b, SignByFun[op](a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, x and *")
-        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
