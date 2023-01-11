320f0a89cdc0e7d6969ab9ed69fde1b90f08e9ea
root <thembamashabane0@gmail.com>
2023-01-11 08:22:30 -0800

    python inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Module for class MyList"""
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """class MyList that inherits from list"""
+    """Implements sorted printing for the built-in list class."""
+
     def print_sorted(self):
+        """Print a list in sorted ascending order."""
         print(sorted(self))
--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
-class MyInt(int):
-    """Module for class MyInt"""
+"""Defines a class MyInt that inherits from int."""
 
+
+class MyInt(int):
+    """Invert int operators == and !=."""
 
     def __eq__(self, value):
-        """Swap == with !=
-
-        Args:
-            value: object to compare
-        Returns: True if value and self are differents
-                False in otherwise
-        """
-        return super().__ne__(value)
+        """Override == opeartor with != behavior."""
+        return self.real != value
 
     def __ne__(self, value):
-        """Swap != with ==
-
-        Args:
-            value: object to compare
-        Returns: False if value and self are iqual
-                True in otherwise
-        """
-        return super().__eq__(value)
+        """Override != operator with == behavior."""
+        return self.real == value
