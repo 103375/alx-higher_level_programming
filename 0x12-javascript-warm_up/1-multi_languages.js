b952ab158c9fe97bb42072c4ff4dc2ad7eedc8ab
root <thembamashabane0@gmail.com>
2023-03-13 04:17:38 -0700

    javascript warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+console.log(myVar[0]);
+console.log(myVar[1]);
+console.log(myVar[2]);
